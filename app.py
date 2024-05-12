2b5da1d68fbb9f72aad8b620818d82f4d6afa3f0
Fernando Santiago <fernando.santiago@bitwaysystem.com.br>
2024-05-12 20:59:44 -0300

    Fix network and andd Cluster

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import requests
 app = Flask(__name__)
 api = Api(app, version='1.0', title='API Analise de Crédito Quantum Finance',
           description='Uma API simples para calcular a propensão à inadimplência e fazer predições')
-
 ns = api.namespace('Inadimplencia', description='Operações de inadimplência')
-
 # Modelo esperado para entrada de dados, com exemplo
 data_model = api.model('Data', {
     'loan_amount': fields.Integer(required=True, description='Quantia do empréstimo'),
@@ -37,7 +35,7 @@ model = api.model('PropensaoInadimplencia', {
 
 @ns.route('/propensao')
 class PropensaoInadimplencia(Resource):
-    @api.expect(model)
+    @api.expect(data_model)
     def post(self):
         """Calcula a propensão à inadimplência com base nos dados fornecidos"""
         if request.is_json:
@@ -48,9 +46,24 @@ class PropensaoInadimplencia(Resource):
                 prediction = response.json()
                 return {'mensagem': 'Dados recebidos e processados com sucesso', 'predicao': prediction}
             else:
-                api.abort(500, 'Erro ao chamar a API de predição')
+                return api.abort(500, 'Erro ao chamar a API de predição')
+
+@ns.route('/clustering')
+class CustomerClustering(Resource):
+    @api.expect(data_model)
+    def post(self):
+        """Envia os dados diretamente para a API de Clustering para obter a classificação de clustering"""
+        if request.is_json:
+            data = request.json
+            # Correção da chamada à API de Clustering
+            cluster_response = requests.post('http://localhost:5002/Clustering/cluster', json=data)
+            if cluster_response.status_code == 200:
+                cluster_info = cluster_response.json()
+                return {'mensagem': 'Clustering realizado com sucesso', 'cluster_info': cluster_info}, 200
+            else:
+                return api.abort(500, 'Erro ao chamar a API de clustering')
         else:
-            api.abort(400, 'Requisição inválida. O corpo da requisição deve estar no formato JSON')
+            return api.abort(400, 'Requisição inválida. O corpo da requisição deve estar no formato JSON')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
